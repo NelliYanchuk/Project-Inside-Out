6a14f5f355f0ab4f837b1a38e41ec66fd3848a2e
Maria Vasilenko <snysic1987@gmail.com>
2024-10-28 15:56:09 +0100

    first commit

---
+++ src/main/java/out/factoriaf5/out_java/App.java
@@ -0,0 +1,17 @@
+package out.factoriaf5.out_java;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
