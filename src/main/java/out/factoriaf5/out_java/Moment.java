8c61ecbdd2d6eb2bf34a76c9c16804dcabfc4427
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-30 15:48:04 +0100

    ci: format Moment class

--- src/main/java/out/factoriaf5/out_java/Moment.java
+++ src/main/java/out/factoriaf5/out_java/Moment.java
@@ -2,7 +2,7 @@ package out.factoriaf5.out_java;
 
 import java.util.Date;
 
-public class Moment{
+public class Moment {
     private int id;
     private String title;
     private String description;
@@ -10,6 +10,7 @@ public class Moment{
     private Date dateOfMoment;
     private Date dateOfCreation;
     private Date dateOfModification;
+
     // Constructor
     public Moment(int id, String title, String description, String emotion,
             Date dateOfMoment, Date dateOfCreation, Date dateOfModification) {
@@ -21,50 +22,65 @@ public class Moment{
         this.dateOfCreation = dateOfCreation;
         this.dateOfModification = dateOfModification;
     }
+
     // Getters
     public int getId() {
         return id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public String getEmotion() {
         return emotion;
     }
+
     public Date getDateOfMoment() {
         return dateOfMoment;
     }
+
     public Date getDateOfCreation() {
         return dateOfCreation;
     }
+
     public Date getDateOfModification() {
         return dateOfModification;
     }
+
     // Setters
     public void setId(int id) {
         this.id = id;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public void setEmotion(String emotion) {
         this.emotion = emotion;
     }
+
     public void setDateOfMoment(Date dateOfMoment) {
         this.dateOfMoment = dateOfMoment;
     }
+
     public void setDateOfCreation(Date dateOfCreation) {
         this.dateOfCreation = dateOfCreation;
     }
+
     public void setDateOfModification(Date dateOfModification) {
         this.dateOfModification = dateOfModification;
     }
+
     public void setAll(int id,
             String title,
             String description,
@@ -80,7 +96,4 @@ public class Moment{
         this.dateOfCreation = dateOfCreation;
         this.dateOfModification = dateOfModification;
     }
-    
-
-
 }
