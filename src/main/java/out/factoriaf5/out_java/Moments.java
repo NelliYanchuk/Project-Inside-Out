b2c0a05e2750cd9b4484e04eb93225755c3f6e68
NelliYanchuk <yanchuk.nelli@gmail.com>
2024-10-30 10:19:20 +0100

    ci: add inits and constructor

--- src/main/java/out/factoriaf5/out_java/Moments.java
+++ src/main/java/out/factoriaf5/out_java/Moments.java
@@ -1,5 +1,25 @@
 package out.factoriaf5.out_java;
 
+import java.text.SimpleDateFormat;
+
 public class Moments {
- 
+  private int id;
+    private String title;
+    private String description;
+    private String emotion;
+    private SimpleDateFormat dateOfMoment;
+    private SimpleDateFormat dateOfCreation;
+    private SimpleDateFormat dateOfModification;
+
+    // Constructor
+    public Moments(int id, String title, String description, String emotion,
+            SimpleDateFormat dateOfMoment, SimpleDateFormat dateOfCreation, SimpleDateFormat dateOfModification) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.emotion = emotion;
+        this.dateOfMoment = dateOfMoment;
+        this.dateOfCreation = dateOfCreation;
+        this.dateOfModification = dateOfModification;
+    }
 }
